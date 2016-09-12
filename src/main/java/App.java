6237fc1390d35ac76695b2e90f7053cc6db8dfa6
Matt Nicholas and Sara Jensen <matt+sara@>
2016-09-12 09:41:40 -0700

    Add form and greeting card template

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,6 +10,7 @@ public class App{
   public static void main(String[] args){
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
+
     get("/", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
       model.put("template", "templates/hello.vtl" );
@@ -19,6 +20,22 @@ public class App{
     get("/favorite_photos", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
       model.put("template", "templates/favorite_photos.vtl" );
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/form", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/greeting_card", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      String recipient = request.queryParams("recipient");
+      String sender = request.queryParams("sender");
+      model.put("recipient", recipient);
+      model.put("sender", sender);
+      model.put("template", "templates/greeting_card.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
   }
